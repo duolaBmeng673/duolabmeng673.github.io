08a9b717a2f246bdf7323665b2fca8099f2b01ce
Yang Yufei <1447807745@qq.com>
2026-07-09 15:26:45 +0800

    Initial Astro blog

---
+++ src/config.ts
@@ -0,0 +1,8 @@
+export const siteConfig = {
+  name: '杨雨飞',
+  title: '杨雨飞的博客',
+  description: '哈尔滨工业大学信息安全专业，关注 Web 开发、分布式系统、AI 与算法。',
+  education: '哈尔滨工业大学 信息安全专业',
+  github: 'https://github.com/duolaBmeng673',
+  tags: ['Web 开发', '分布式系统', 'AI', '算法', 'CMU'],
+};
